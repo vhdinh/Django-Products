af59d8d87bf5267951f5ecd3161c246b55c04ee0
Vu Dinh <isthis.vu@gmail.com>
2016-06-21 16:49:01 -0700

    added product model and create page

--- apps/products/models.py
+++ apps/products/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	name = models.CharField(max_length = 100)
+	description = models.TextField()
+	crice = models.DecimalField(max_digits=10, decimal_places=2)
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now=True)
+	def __str__(self):
+		return self.name
+	class Meta():
+		db_table = "products"
